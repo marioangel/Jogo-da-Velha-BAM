902686c1470b91a92756be54c33e11bbecc42fc6
Rodrigo Barros Bernardino <rbbernardino@gmail.com>
2010-11-06 12:27:36 -0400

    Adicionado módulo para exemplificar o padrão dos códigos

---
+++ bam_tui.erl
@@ -0,0 +1,39 @@
+%% BAM - Bernardino Anderson Mário
+%%       
+%% Disciplina: Engenharia de Software 2010/2
+%% Professor:  Jucimar Jr ( jucimar.jr@gmail.com )
+%% Alunos : Anderson Pimentel
+%%          Mário Angel
+%%          Rodrigo Bernardino ( rbbernardino@gmail.com )
+%%
+%% Objetivo : Módulo da interface texto
+
+-module(bam_tui).
+-vsn(0.11).
+
+-author('rbbernardino@gmail.com').
+-author('mr.garcia1@hotmail.com').
+-author('andbrain@gmail.com').
+
+-export([init/0, sel_op/0, ins_nome/0,
+	sel_nivel/0, ativo/0]).
+
+%%-----------------------------------------------------------------------------
+%% init()
+%%   descricao da funcao, metodo utilizado, etc
+%%   nao faca linha muito grandes, respeite o limite de 80 caracteres, ou seja,
+%%   a linha com "-" acima
+init() ->
+    ok.
+
+sel_op() ->
+    ok.
+
+ins_nome() ->
+    ok.
+
+sel_nivel() ->
+    ok.
+
+ativo() ->
+    ok.
