747750c8cd0c4a7ee0e25a4d90f69cc349cdb688
Mario Angel <mr.garcia1@hotmail.com>
2010-11-09 15:25:59 -0400

    Funcao init finalizada[bam_tui]

--- bam_tui.erl
+++ bam_tui.erl
@@ -3,7 +3,7 @@
 %% Disciplina: Engenharia de Software 2010/2
 %% Professor:  Jucimar Jr ( jucimar.jr@gmail.com )
 %% Alunos : Anderson Pimentel ( andbrain@gmail.com )
-%%          Mário Angel
+%%          Mário Angel(mr.garcia1@hotmail.com)
 %%          Rodrigo Bernardino ( rbbernardino@gmail.com )
 %%
 %% Objetivo : Módulo da interface texto
@@ -19,13 +19,28 @@
 	sel_nivel/0, ativo/0]).
 
 %%-----------------------------------------------------------------------------
-%% init()
-%%   descricao da funcao, metodo utilizado, etc
+%%init()
+%%Imprimi o cabecalho inicial e pergunta se deseja ou nao comecar uma partida 
+init() ->
+	io:format(
+	"\t\t\t INICIANDO JOGO DA VELHA BAM \n\n"++
+	"\t\t      Autores:Mario, Rodrigo, Anderson \n\n"),
+
+	io:format(
+	"Jogo da velha BAM \n"++"(1) Jogar \n"++"(2) Sair\n"),
+
+	Read = io: fread("Escolha Opcao: ","~d"),
+	case Read of
+		{ok,[1]} -> sel_oponente();
+		{ok,[2]} -> sair();
+		{ok, _} -> io:format("\nOPCAO INVALIDA\n"++"TENTE NOVAMENTE\n"),
+				init()	
+	end.
+%%-----------------------------------------------------------------------------
+%%sel_oponente()
+%%   funcao, metodo utilizado, etc
 %%   nao faca linha muito grandes, respeite o limite de 80 caracteres, ou seja,
 %%   a linha com "-" acima
-init() ->
-    ok.
-
 sel_oponente() ->
     io:format("Digite o tipo de oponente desejado:~n~n"),
     io:format("(1) Humano x Humano (2) Humano x Maquina (3) Menu Principal~n"),
